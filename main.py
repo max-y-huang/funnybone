9815691d6c7f203b281b829de0e1e002c8352698
Max Huang <letmaxknow@gmail.com>
2022-11-29 00:55:12 -0500

    reverted to sqlite

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sqlite3
 import requests
 import json
 import urllib.parse
 from flask import Flask, request, render_template
-from google.cloud.sql.connector import Connector
-from scripts.sql import get_pool
+
+_DB_SRC = "output.db"
 
 
 app = Flask(__name__)
@@ -19,7 +20,7 @@ def index():
     encoded_query = None if not query else urllib.parse.quote(query)
 
     # get funniest and unfunniest matches
-    ranked_terms, _ = get_rankings(query)
+    ranked_terms = get_rankings(query)
     ranked_terms = list(map(lambda x: x.replace("_", " "), ranked_terms))
     funniest = ranked_terms[:3]
     unfunniest = ranked_terms[-1:]
@@ -61,18 +62,19 @@ def get_rankings(term):
     if term.lower().replace(" ", "_") in filter_words:
         return ([], [])  # throws error
 
-    connector = Connector()
-    pool = get_pool(connector)
-    with pool.connect() as c:
-        terms_list = ", ".join([f"'{t}'" for t in similar_terms])
-        res = c.execute(f"SELECT * FROM Terms WHERE term IN ({terms_list});")
-        ratings = {key: val for key, val in res.fetchall()}
-    connector.close()
+    conn = sqlite3.connect(_DB_SRC)
+    c = conn.cursor()
 
-    unranked_terms = list(filter(lambda t: not (t in ratings), similar_terms))
+    terms_list = ", ".join([f"'{t}'" for t in similar_terms])
+    res = c.execute(f"SELECT * FROM Terms WHERE term IN ({terms_list});")
+    ratings = {key: val for key, val in res.fetchall()}
+
+    conn.commit()
+    conn.close()
+
     ranked_terms = list(filter(lambda t: t in ratings, similar_terms))
     ranked_terms = sorted(ranked_terms, key=lambda t: ratings[t], reverse=True)
-    return (ranked_terms, unranked_terms)
+    return ranked_terms
 
 
 def get_filter_words():
--- scripts/init_db.py
+++ scripts/init_db.py
@@ -1,12 +1,23 @@
-from google.cloud.sql.connector import Connector
-from sql import get_pool
+import sqlite3
+import json
+
+_INPUT_SRC = "../output.json"
+_OUTPUT_SRC = "../output.db"
 
 if __name__ == "__main__":
-    connector = Connector()
-    pool = get_pool(connector)
-    with pool.connect() as c:
-        c.execute("DROP TABLE IF EXISTS Terms;")
-        c.execute(
-            "CREATE TABLE IF NOT EXISTS Terms (term VARCHAR(255) NOT NULL, score FLOAT NOT NULL, PRIMARY KEY (term));"
-        )
-    connector.close()
+    conn = sqlite3.connect(_OUTPUT_SRC)
+    c = conn.cursor()
+
+    c.execute(f"CREATE TABLE IF NOT EXISTS Terms (term TEXT, score REAL)")
+    c.execute("CREATE INDEX IF NOT EXISTS index1 ON Terms (term)")
+    c.execute("DELETE FROM Terms")
+
+    with open(_INPUT_SRC, "r") as f:
+        data = json.load(f)
+        for entry in data:
+            c.execute(
+                "INSERT INTO Terms VALUES (?, ?)", (entry["term"], entry["score"])
+            )
+
+    conn.commit()
+    conn.close()
